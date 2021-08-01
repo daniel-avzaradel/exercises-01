75dcc88572365b049fa568a267639b41a89f7d66
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-08-01 05:09:08 +0300

    using spread operator

--- react-course-tutorial/react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const changeMessage = (person) => {
+    if (person.message === "random message") {
+      console.log(person.message);
+      setPerson({ ...person, message: "hello world" });
+    } else {
+      setPerson({ ...person, message: "random message" });
+    }
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={() => changeMessage(person)}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
