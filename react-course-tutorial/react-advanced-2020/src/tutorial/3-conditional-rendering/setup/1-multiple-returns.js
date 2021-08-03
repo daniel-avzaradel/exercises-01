003ecf3d4e495bcb493944e70b5ec6ee8ce5bba0
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-08-03 07:13:45 +0300

    practising useEffect with fetch to display data

--- react-course-tutorial/react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,38 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState(["default user"]);
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    setUser(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h2>loading...</h2>;
+  }
+
+  const { id, name, avatar_url, login, html_url } = user;
+
+  return (
+    <ul>
+      <div className="items">
+        <img src={avatar_url} alt={login} />
+        <h4>{name}</h4>
+        <a href={html_url}>Profile</a>
+      </div>
+    </ul>
+  );
 };
 
 export default MultipleReturns;
