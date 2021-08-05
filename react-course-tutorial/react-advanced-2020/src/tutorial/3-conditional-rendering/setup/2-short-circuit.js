82175e0ed141eb96b546cafc61af2970d8bb2f1c
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-08-05 22:57:19 +0300

    created error display with conditional rendering

--- react-course-tutorial/react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,15 +4,17 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false)
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
   return (
     <>
     {/* <h1>{firstValue}</h1>
     <h1>value: {secondValue}</h1> */}
     <h1>{text || 'John Doe'}</h1>
-    <h1>{text || firstValue}</h1>
+    <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+    <h1>{isError && <h1>Error...</h1>}</h1>
     </>
   )
 };
