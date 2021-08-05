24a29754f18164caf665de4a6686c242c34d4780
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-08-06 02:22:01 +0300

    added onChange handler to inputs

--- react-course-tutorial/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -7,17 +7,35 @@ import React, { useState } from 'react';
 
 const ControlledInputs = () => {
 
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const Submit = (e) => {
+    e.preventDefault();
+    console.log('hello world');
+  }
+
+  const handleChangeName = (e) => {
+    setFirstName(e.target.value)
+    console.log(e.target.value);
+  }
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+    console.log(e.target.value);
+  }
+
   return (
     <>
     <article>
-      <form className='form'>
+      <form className='form' onSubmit={(e) => Submit(e)}>
         <div className="form-control">
           <label htmlFor="name">Name: </label>
-          <input type="text" id='firstName' name='firstName' />
+          <input type="text" id='firstName' name='firstName' value={firstName} onChange={(e) => handleChangeName(e)}/>
         </div>
         <div className="form-control">
           <label htmlFor="email">Email: </label>
-          <input type="text" id='email' name='email' />
+          <input type="text" id='email' name='email' value={email} onChange={(e) => handleChangeEmail(e)}/>
         </div>
         <button type='submit'>add person</button>
       </form>
