41085e280599567f0e6a590e57ff5828b772f846
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-08-06 01:47:49 +0300

    started exercise show/hide in react

--- react-course-tutorial/react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -14,7 +14,8 @@ const ShortCircuit = () => {
     <h1>value: {secondValue}</h1> */}
     <h1>{text || 'John Doe'}</h1>
     <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
-    <h1>{isError && <h1>Error...</h1>}</h1>
+    {isError && <h1>Error...</h1>}
+    {isError ? <p>error happened...</p> : <p>sky is the limit</p> }
     </>
   )
 };
