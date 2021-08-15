e98d41b1e2d7a3c9be8ecfaa8ead71c879134eda
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-08-15 09:40:00 +0300

    started using route and switch

--- use-reducer/src/App.js
+++ use-reducer/src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Switch, Link } from "react-router-dom";
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const MyProjects = () => {
+  return <h1>My Projects</h1>;
+};
 
 function App() {
   return (
     <div>
       <h1>APP</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/myproject" component={MyProjects} />
+      </Switch>
     </div>
   );
 }
