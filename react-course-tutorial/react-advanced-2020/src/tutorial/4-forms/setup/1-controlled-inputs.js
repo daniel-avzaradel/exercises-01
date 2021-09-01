c37ef28124349b09928350fe03b5ffc10c4e2b57
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-01 12:40:01 +0300

    controlled inputs exercise

--- react-course-tutorial/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,10 +6,9 @@ import React, { useState, useEffect } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
-  const [people, setPeople] = useState([])
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
 
   useEffect(() => {
     console.log(people);
@@ -17,45 +16,61 @@ const ControlledInputs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(firstName && email) {
-      const person = { firstName: firstName, email: email };
+    if (firstName && email) {
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName,
+        email,
+      };
 
       setPeople((people) => {
-          return [...people, person]
+        return [...people, person];
       });
-      setFirstName('');
-      setEmail('')
-
+      setFirstName("");
+      setEmail("");
     } else {
-      console.log('empty values');
+      console.log("empty values");
     }
-  }
+  };
 
   return (
     <>
-    <article>
-      <form className='form' onSubmit={(e) => handleSubmit(e)}>
-        <div className="form-control">
-          <label htmlFor="name">Name: </label>
-          <input type="text" id='firstName' name='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-        </div>
-        <div className="form-control">
-          <label htmlFor="email">Email: </label>
-          <input type="text" id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <button type='submit'>add person</button>
-      </form>
-      {people.map((person, i) => {
-        const { firstName, email } = person;
-        return(
-          <div key={i}>
-            <p>{firstName}, {email}</p>
+      <article>
+        <form className="form" onSubmit={(e) => handleSubmit(e)}>
+          <div className="form-control">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
-        )
-      })}
-    </article>
+          <div className="form-control">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
+      </article>
     </>
-  )
+  );
 };
 
 export default ControlledInputs;
