241c75a63501435e3fe91e61a389387ee92b85d6
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-11 05:02:36 +0300

    using useEffect to trigger Focus on the input on render

--- react-course-tutorial/react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -6,21 +6,31 @@ import React, { useEffect, useRef } from "react";
 
 const UseRefBasics = () => {
   const refContainer = useRef(null);
+  const divContainer = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(refContainer.current.value);
+    console.log(divContainer.current);
   };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
 
   console.log(refContainer);
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <div>
-        <input type="text" ref={refContainer} />
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>Hello World</div>
+    </>
   );
 };
 
