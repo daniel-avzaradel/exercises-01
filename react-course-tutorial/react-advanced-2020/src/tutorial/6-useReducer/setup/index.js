0b609125beae4a7528b5adc027a99cd21046411b
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-11 06:29:41 +0300

    moved reducer to a new file

--- react-course-tutorial/react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ react-course-tutorial/react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -2,46 +2,8 @@ import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
+import reducer from "./reducer";
 
-const reducer = (state, action) => {
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [
-      ...state.people,
-      { id: new Date().getTime().toString(), name: action.payload },
-    ];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item added",
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return {
-      ...state,
-      isModalOpen: true,
-      modalContent: "Please enter a value",
-    };
-  }
-  if (action.type === "CLOSE_MODAL") {
-    return {
-      ...state,
-      isModalOpen: false,
-    };
-  }
-  if (action.type === "REMOVE_ITEM") {
-    const removePerson = state.people.filter((person) => {
-      return person.id !== action.payload;
-    });
-    return {
-      ...state,
-      people: removePerson,
-      isModalOpen: true,
-      modalContent: "Item removed",
-    };
-  }
-  throw new Error("no matching action type");
-};
 const defaultState = {
   people: [],
   isModalOpen: false,
