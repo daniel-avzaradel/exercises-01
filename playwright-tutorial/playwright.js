d447a28ecdb87b56c201944db7c08cf792d934be
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-17 08:55:32 +0300

    for loop inside dropdown options

--- playwright-tutorial/playwright.js
+++ playwright-tutorial/playwright.js
@@ -2,18 +2,23 @@ const { chromium } = require("playwright");
 
 (async () => {
   // function code
-  const browser = await chromium.launch({ headless: false, slowMo: 300 });
+  const browser = await chromium.launch({ headless: false, slowMo: 1000 });
   const page = await browser.newPage();
 
   await page.goto("https://the-internet.herokuapp.com/dropdown");
   const dropdown = await page.$("#dropdown");
 
   // value
-  await dropdown.selectOption({ value: 1 });
+  await dropdown.selectOption({ value: "1" });
   // label
   await dropdown.selectOption({ label: "Option 2" });
   // index
-  await dropdown.selectOption({ index: 2 });
+  await dropdown.selectOption({ index: 0 });
+  // values inside the select
+  const availableOptions = await dropdown.$$("option");
+  for (let i = 0; i < availableOptions.length; i++) {
+    console.log(await availableOptions[i].innerText());
+  }
 
   await page.close();
 })();
