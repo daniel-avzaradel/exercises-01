eee764e08f4870723f9b0f63968315ded3eae371
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-17 08:29:30 +0300

    playwright select from dropdown

--- playwright-tutorial/playwright.js
+++ playwright-tutorial/playwright.js
@@ -2,16 +2,18 @@ const { chromium } = require("playwright");
 
 (async () => {
   // function code
-  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+  const browser = await chromium.launch({ headless: false, slowMo: 300 });
   const page = await browser.newPage();
-  await page.goto("http://google.com");
 
-  const search = await page.$(
-    "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input"
-  );
-  await search.type("YouTube", { delay: 50 });
+  await page.goto("https://the-internet.herokuapp.com/dropdown");
+  const dropdown = await page.$("#dropdown");
 
-  await page.keyboard.press("Enter", { delay: 3000 });
+  // value
+  await dropdown.selectOption({ value: 1 });
+  // label
+  await dropdown.selectOption({ label: "Option 2" });
+  // index
+  await dropdown.selectOption({ index: 2 });
 
   await page.close();
 })();
