0448dd366f7d2278ff516706dc445d91390d046b
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-27 01:26:12 +0300

    finished project 05-menus and refactored code forc map method on categories

--- react-projects/05-menu/setup/src/App.js
+++ react-projects/05-menu/setup/src/App.js
@@ -3,13 +3,23 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = [
+  "all",
+  ...new Set(
+    items.map((item) => {
+      return item.category;
+    })
+  ),
+];
+
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
   const filterItems = (category) => {
     if (category === "all") {
-      console.log("items, ", items);
       setMenuItems(items);
       return;
     }
@@ -24,7 +34,11 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} menuItems={menuItems} />
+        <Categories
+          filterItems={filterItems}
+          menuItems={menuItems}
+          allCategories={allCategories}
+        />
         <Menu items={menuItems} />
       </section>
     </main>
--- react-projects/05-menu/setup/src/Categories.js
+++ react-projects/05-menu/setup/src/Categories.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ filterItems, allCategories }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("all")}>
-        all
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-        breakfast
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("lunch")}>
-        lunch
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("shakes")}>
-        shakes
-      </button>
+      {allCategories.map((item, id) => {
+        return (
+          <button
+            className="filter-btn"
+            onClick={() => filterItems(item)}
+            key={id}
+          >
+            {item}
+          </button>
+        );
+      })}
     </div>
   );
 };
