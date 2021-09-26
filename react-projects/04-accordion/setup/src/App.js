be607fcf23eefd1514447b6a0f158017ebb4738b
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-09-26 21:10:39 +0300

    finished project number 04 - Accordion questions

--- react-projects/04-accordion/setup/src/App.js
+++ react-projects/04-accordion/setup/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div className="container">
+      <h3>Questions and answers about login</h3>
+      <section className="info">
+        {questions.map((item) => {
+          return <SingleQuestion {...item} />;
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default App;
--- react-projects/04-accordion/setup/src/Question.js
+++ react-projects/04-accordion/setup/src/Question.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = (item) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="question">
+      <header>
+        <h4>{item.title} </h4>
+        <button className="btn" onClick={() => setOpen(!open)}>
+          {open ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {open ? <span>{item.info}</span> : <span></span>}
+    </div>
+  );
 };
 
 export default Question;
