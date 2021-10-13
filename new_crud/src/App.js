e0b5d39c007f3a3c0befa16a7ceb16b2c324546a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-10-13 23:35:07 +0300

    created new-crud project

---
+++ new_crud/src/App.js
@@ -0,0 +1,12 @@
+import Layout from "./components/Layout/Layout";
+import Form from "./components/Utils/Form";
+
+function App() {
+  return (
+    <Layout>
+      <Form></Form>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ new_crud/src/components/Utils/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Form.module.css";
+
+function Form() {
+  return (
+    <div className={styles.form__page}>
+      <div className={styles.form__container}>Form</div>
+    </div>
+  );
+}
+
+export default Form;
