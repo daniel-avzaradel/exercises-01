085c8552479867a369f5686e89ea44699039aadc
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-10-14 19:59:32 +0300

    created different folders

--- new_crud_app/server/server.js
+++ new_crud_app/server/server.js
@@ -1,30 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("../server/database/connection");
 const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config({ path: "config.env" });
-
-const connectDB = async () => {
-  try {
-    const con = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`MongoDB connected: ${con.connection.host}`);
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-};
 
 connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("request successful");
-});
+app.get("/", (req, res) => {});
 
 app.listen(3030, () => {
   console.log("listening to port 3030");
