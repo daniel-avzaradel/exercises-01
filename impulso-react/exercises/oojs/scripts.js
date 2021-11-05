5a81befa471929bdaac4dd00ddeb63010f8cffd9
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-11-05 07:48:57 +0200

    finished exercises with JS Classes

--- impulso-react/exercises/oojs/scripts.js
+++ impulso-react/exercises/oojs/scripts.js
@@ -1,9 +1,9 @@
 class ContaBancaria {
-  constructor(agencia, numero, tipo, saldo) {
+  constructor(agencia, numero, tipo) {
     this.agencia = agencia;
     this.numer = numero;
     this.tipo = tipo;
-    this._saldo = saldo;
+    this._saldo = 0;
   }
 
   get saldo() {
@@ -28,9 +28,9 @@ class ContaBancaria {
   }
 }
 
-class ContaCorrente {
-  constructor(agencia, numero, saldo, cartaoCredito) {
-    super(agencia, numero, saldo);
+class ContaCorrente extends ContaBancaria {
+  constructor(agencia, numero, cartaoCredito) {
+    super(agencia, numero);
     this.tipo = "corrente";
     this._cartaoCredito = cartaoCredito;
   }
@@ -41,5 +41,27 @@ class ContaCorrente {
 
   set cartaoCredito(valor) {
     this._cartaoCredito = valor;
+  }
+}
+
+class ContaPoupanca extends ContaBancaria {
+  constructor(agencia, numero) {
+    super(agencia, numero);
+    this.tipo = "poupança";
+  }
+}
+
+class ContaUniversitaria extends ContaBancaria {
+  constructor(agencia, numero) {
+    super(agencia, numero);
+    this.tipo = "universitária";
+  }
+
+  sacar(valor) {
+    if (valor > 500) {
+      return "Operação negada. Valor máximo de: R$500";
+    }
+
+    this._saldo = this._saldo - valor;
   }
 }
