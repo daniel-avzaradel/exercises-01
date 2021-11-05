eb8f0d990c515383470f476be22a6a58b3a9ca53
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-11-05 12:33:32 +0200

    refactored code for fetch API exercise

--- impulso-react/exercises/js-async/assets/js/scripts.js
+++ impulso-react/exercises/js-async/assets/js/scripts.js
@@ -1,13 +1,22 @@
 const BASE_URL = "https://thatcopy.pw/catapi/rest";
-const img = document.querySelector("#cat");
+const catImg = document.querySelector("#cat");
+const loadBtn = document.querySelector("#change-cat");
 
 const getCats = async () => {
   try {
     const data = await fetch(BASE_URL);
     const res = await data.json();
-    img.src = res.webpurl;
+
     return res.webpurl;
   } catch (err) {
     console.log(err);
   }
 };
+
+const loadImg = async () => {
+  catImg.src = await getCats();
+};
+
+loadBtn.addEventListener("click", () => {
+  loadImg();
+});
