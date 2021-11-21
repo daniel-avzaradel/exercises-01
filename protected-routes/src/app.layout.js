c7103ab74cbfa79dc220e610608786b6a26d53ba
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-11-21 22:34:22 +0200

    changed syntax to match new react-router-dom v6

--- protected-routes/src/app.layout.js
+++ protected-routes/src/app.layout.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppLayout = () => {
-  return <div>App Layout</div>;
+  let navigate = useNavigate();
+
+  return (
+    <div className="landing_page">
+      <h1>App Layout</h1>
+      <button
+        className="btn"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        {`<< `}Go Back
+      </button>
+    </div>
+  );
 };
