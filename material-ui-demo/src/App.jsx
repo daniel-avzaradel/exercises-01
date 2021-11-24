841353f4716c17954200e26a54b701841a058fdf
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-11-24 20:46:08 +0200

    learning how to style material-ui components in React

--- material-ui-demo/src/App.jsx
+++ material-ui-demo/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 
@@ -21,8 +21,26 @@ function App() {
                         <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
                             Photo Album
                         </Typography>
+                        <Typography variant='h5' align='center' color='textSecondary' paragraph>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae est, repudiandae ipsum minima aliquid exercitationem hic laborum facilis dolorum esse, tempore numquam, perspiciatis quidem nostrum explicabo sunt quas error illum.
+                        </Typography>
+                        <div>
+                            <Grid container spacing={2} justify='center'>
+                                <Grid item>
+                                    <Button variant='contained' color='primary'>
+                                        See my photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='outlined' color='primary'>
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
+               
             </main>
         </>
     )
