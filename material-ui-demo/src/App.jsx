23b1a7ec764fcaf90ad507caf6a8f7d1ed3553aa
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-11-24 18:46:53 +0200

    creating material-ui demo project

---
+++ material-ui-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@material-ui/core'
+
+function App() {
+    return (
+        <div>
+            <Typography variant='h1'>Hello World!</Typography>
+        </div>
+    )
+}
+
+export default App
